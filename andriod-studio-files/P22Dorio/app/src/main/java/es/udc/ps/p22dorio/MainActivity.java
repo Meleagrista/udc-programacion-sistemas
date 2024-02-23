d366770b7f8fe2954742664d707f57d8f1bdbb11
Martín do Río Rico <75792048+Meleagrista@users.noreply.github.com>
2024-02-23 20:56:32 +0100

    23/02/2024: Finished second practice.

--- andriod-studio-files/P22Dorio/app/src/main/java/es/udc/ps/p22dorio/MainActivity.java
+++ andriod-studio-files/P22Dorio/app/src/main/java/es/udc/ps/p22dorio/MainActivity.java
@@ -1,40 +1,59 @@
 package es.udc.ps.p22dorio;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    String TAG = "_TAG";
+
+    // TAG for logging
+    static String TAG = "_TAG";
+    // Name of the activity
     String ACTIV = "MainActivity";
+    // Key for passing text between activities
     static String KEY_TEXT = "asdfgh";
 
+    // Buttons and views
     Button share_button, continue_button;
+    TextView text_view;
     EditText edit_text;
 
+    // Activity result launcher for handling activity results
+    private ActivityResultLauncher<Intent> myStartActivityForResult;
+
+    // Handling click events
     @Override
     public void onClick(View v) {
         String text = edit_text.getText().toString();
 
-        if (! text.isEmpty()) {
-            if (v == share_button)
-            {
+        if (!text.isEmpty()) {
+            if (v == share_button) {
+                // Share button clicked
                 Log.d(TAG, "Botón compartir");
+                Intent intentSend = new Intent(Intent.ACTION_SEND);
+                intentSend.setType("text/plain");
+                intentSend.putExtra(Intent.EXTRA_TEXT, text);
+                startActivity(intentSend);
             } else if (v == continue_button) {
-                Log.d(TAG, "Botón continuar");
+                // Continue button clicked
                 Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
                 intent.putExtra(KEY_TEXT, text);
-                startActivity(intent);
+                myStartActivityForResult.launch(intent);
             } else {
+                // Unknown button clicked
                 Log.d(TAG, "Fallo en el botón");
             }
         } else {
+            // Text is empty
             Toast.makeText(this, R.string.empty_text, Toast.LENGTH_LONG).show();
         }
     }
@@ -45,21 +64,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Log.d(TAG, ACTIV + " onCreate");
 
+        // Initialize views
         edit_text = findViewById(R.id.edit_text);
+        text_view = findViewById(R.id.main_text_view);
         share_button = findViewById(R.id.share_button_id);
-        share_button.setOnClickListener(this);
         continue_button = findViewById(R.id.continue_button_id);
+
+        // Set click listeners
+        share_button.setOnClickListener(this);
         continue_button.setOnClickListener(this);
 
-        /* button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, ACTIV + " onClick");
-                ((Button) v).setText(R.string.share_button);
-            }
-        }); */
+        // Initialize the ActivityResultLauncher
+        myStartActivityForResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == AppCompatActivity.RESULT_OK) {
+                        Intent data = result.getData();
+                        Bundle bundle = data != null ? data.getExtras() : null;
+                        if (bundle != null) {
+                            String str_text = bundle.getString(KEY_TEXT, getString(R.string.void_text));
+                            text_view.setText(str_text);
+                        }
+                    }
+                }
+        );
     }
 
+    // Lifecycle methods
     @Override
     protected void onStart() {
         super.onStart();
--- andriod-studio-files/P22Dorio/app/src/main/java/es/udc/ps/p22dorio/SecondActivity.java
+++ andriod-studio-files/P22Dorio/app/src/main/java/es/udc/ps/p22dorio/SecondActivity.java
@@ -1,25 +1,99 @@
 package es.udc.ps.p22dorio;
 
 import static es.udc.ps.p22dorio.MainActivity.KEY_TEXT;
+import static es.udc.ps.p22dorio.MainActivity.TAG;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.TextView;
-
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView text_view;
+
+    Button button1, button2, button3;
+    // Name of the activity
+    String ACTIV = "SecondActivity";
+
+    @Override
+    public void onClick(View v) {
+        if (v == button1) {
+            Log.d(TAG, "Botón 1");
+            Intent intent = new Intent();
+            intent.putExtra(KEY_TEXT, ((Button) v).getText().toString());
+            setResult(RESULT_OK, intent);
+            finish();
+        } else if (v == button2) {
+            Log.d(TAG, "Botón 2");
+            Intent intent = new Intent();
+            intent.putExtra(KEY_TEXT, ((Button) v).getText().toString());
+            setResult(RESULT_OK, intent);
+            finish();
+        } else if (v == button3) {
+            Log.d(TAG, "Botón 3");
+            Intent intent = new Intent();
+            intent.putExtra(KEY_TEXT, ((Button) v).getText().toString());
+            setResult(RESULT_OK, intent);
+            finish();
+        } else {
+            Log.d(TAG, "Fallo en el botón");
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        Log.d(TAG, ACTIV + " onCreate");
+
         text_view = findViewById(R.id.text_view);
+        button1 = findViewById(R.id.button_1);
+        button1.setOnClickListener(this);
+        button2 = findViewById(R.id.button_2);
+        button2.setOnClickListener(this);
+        button3 = findViewById(R.id.button_3);
+        button3.setOnClickListener(this);
+
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             String text = bundle.getString(KEY_TEXT, getString(R.string.empty_text));
             text_view.setText(text);
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, ACTIV + " onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, ACTIV + " onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, ACTIV + " onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, ACTIV + " onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, ACTIV + " onDestroy");
     }
 }
